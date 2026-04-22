13f949e5e7e73536bd57d8946913c83b52e4ed60
Nicolás Spagnolo <nicolasspagnolo2004@gmail.com>
2026-04-21 23:57:49 -0300

    form abm usuario agregado, falta corregir

---
+++ SistemaBase/Administracion/FormABMUsuario_625NS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaBase.Administracion
+{
+    public partial class FormABMUsuario_625NS : Form
+    {
+        public FormABMUsuario_625NS()
+        {
+            InitializeComponent();
+        }
+    }
+}
